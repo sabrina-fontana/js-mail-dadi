95d5e63076d1318f521b08b03ae1f2b644ba208b
sabrina-fontana <sabfon@hotmail.it>
2020-12-15 17:27:03 +0100

    esercizio completo corretto

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 // lista delle email autorizzate
 var email = ['sabfon@hotmail.it', 'email@gmail.com', 'email@outlook.it'];
-// chiedo all'utente la sua email
-// var userEmail = prompt('Scrivi il tuo indirizzo email');
-// se non è autorizzato lo fermo, altrimenti procede al gioco
-// for(var i = 0; i < email.length; i++) {
-//   if (userEmail !== email[i]) {
-//   }
 
+// chiedo all'utente la sua email
+var userEmail = prompt('Scrivi il tuo indirizzo email');
 
 // numero random da 1 a 6 per il computer
 var computerNumber = Math.floor(Math.random() * 6) +1;
+
 // numero random da 1 a 6 per l'utente
 var userNumber = Math.floor(Math.random() * 6) +1;
 
-var result = document.getElementById('text');
+var result = document.getElementById('output');
 
-if (computerNumber > userNumber) {
-result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Hai perso';
-} else if (computerNumber < userNumber) {
-  result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Hai vinto!';
+// // VALIDAZIONE EMAIL AVVENUTA?
+var emailValidation = false;
+
+for(var i = 0; i <= email.length; i++) {
+  if (userEmail === email[i]) {
+    emailValidation = true;
+  }
+}
+
+// SE E' AUTORIZZATO PROCEDE AL GIOCO, ALTRIMENTI LO FERMO
+if (emailValidation) {
+  alert('Giochiamo!');
+  if (computerNumber > userNumber) {
+  result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>HAI PERSO';
+  } else if (computerNumber < userNumber) {
+    result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>HAI VINTO!';
+  } else {
+    result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>SIETE PARI';
+  }
 } else {
-  result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Siete pari';
+  alert ('Indirizzo email errato. Riprova');
 }
