4acecb4d15ba35f9a00200f562db4badb4eb8bff
sabrina-fontana <sabfon@hotmail.it>
2020-12-15 16:13:16 +0100

    gioco corretto

--- js/script.js
+++ js/script.js
@@ -4,27 +4,21 @@ var email = ['sabfon@hotmail.it', 'email@gmail.com', 'email@outlook.it'];
 // var userEmail = prompt('Scrivi il tuo indirizzo email');
 // se non è autorizzato lo fermo, altrimenti procede al gioco
 // for(var i = 0; i < email.length; i++) {
-//   if (email[i] !== userEmail) {
-//     alert('Indirizzo errato. Riprova');
+//   if (userEmail !== email[i]) {
 //   }
-// }
 
-// numero random da 1 a 6 generato dal computer
+
+// numero random da 1 a 6 per il computer
 var computerNumber = Math.floor(Math.random() * 6) +1;
-// numero scelto dall'utente
-var userNumber = parseInt(prompt('scegli un numero da 1 a 6'));
+// numero random da 1 a 6 per l'utente
+var userNumber = Math.floor(Math.random() * 6) +1;
 
 var result = document.getElementById('text');
 
-// controllo che l'input dell'utente sia un numero compreso tra 1 e 6
-if (isNaN(userNumber) || userNumber < 1 || userNumber > 6) {
-  alert('Valore errato. Riprova');
+if (computerNumber > userNumber) {
+result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Hai perso';
+} else if (computerNumber < userNumber) {
+  result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Hai vinto!';
 } else {
-  if (computerNumber > userNumber) {
-  result.innerHTML = 'Il computer ha scelto ' + computerNumber + '<br>Hai perso';
-  } else if (computerNumber < userNumber) {
-    result.innerHTML = 'Il computer ha scelto ' + computerNumber + '<br>Hai vinto!';
-  } else {
-    result.innerHTML = 'Entrambi avete scelto ' + computerNumber + '<br>Siete pari';
-  }
+  result.innerHTML = 'Il numero del computer è ' + computerNumber + '<br>Il tuo numero è ' + userNumber + '<br>Siete pari';
 }
